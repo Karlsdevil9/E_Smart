7e102afb481750a77978351f8af07335c7a4cda3
Karlsdevil9 <karlsdevil4@gmail.com>
2020-10-05 08:16:38 +0530

    Custom Painter

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -4,37 +4,73 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 400,
-          height: 300,
-          child: Column(
-            children: [
-              Align(
-                child: Text(
-                  "Hello there,\nwelcome back",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
-                ),
-                alignment: Alignment.topLeft,
+      body: Container(
+        margin: EdgeInsets.fromLTRB(25, 5, 25, 0),
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              color: Colors.white,
+              child: CustomPaint(
+                painter: CurvePainter(),
               ),
-              SizedBox(
-                height: 50,
+            ),
+            SizedBox(
+              height: 150,
+            ),
+            Align(
+              child: Text(
+                "Hello there,\nwelcome back",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
               ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Email'),
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Password'),
-              ),
-              RaisedButton(
-                onPressed: () {},
-                child: Text("Login"),
-              )
-            ],
-          ),
+              alignment: Alignment.topLeft,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Login"),
+            )
+          ],
         ),
       ),
     );
+  }
+}
+
+class CurvePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint();
+    paint.color = Colors.green[800];
+    paint.style = PaintingStyle.fill;
+
+    var path = Path();
+
+    path.moveTo(0, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.25, size.height * 0.875,
+        size.width * 0.5, size.height * 0.9167);
+    path.quadraticBezierTo(size.width * 0.75, size.height * 0.9584,
+        size.width * 1.0, size.height * 0.9167);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
