920f3b6db429b45814b0f5d3576e490f25f92610
Karlsdevil9 <karlsdevil4@gmail.com>
2020-10-06 08:21:13 +0530

    Separate Curvepainter

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './Curvepainter.dart';
+import './SignupScreen.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
@@ -36,36 +38,19 @@ class LoginScreen extends StatelessWidget {
             RaisedButton(
               onPressed: () {},
               child: Text("Login"),
-            )
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                        builder: (BuildContext context) => new SignupScreen()));
+              },
+              child: Text("New here? SignUp instead"),
+            ),
           ],
         ),
       ),
     ));
-  }
-}
-
-class CurvePainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint();
-    paint.color = Colors.green[800];
-    paint.style = PaintingStyle.fill;
-
-    var path = Path();
-
-    path.moveTo(0, size.height * 0.32);
-    path.quadraticBezierTo(size.width * 0.25, size.height * 0.40,
-        size.width * 0.5, size.height * 0.35);
-    path.quadraticBezierTo(size.width * 0.75, size.height * 0.30,
-        size.width * 1.0, size.height * 0.28);
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-
-    canvas.drawPath(path, paint);
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return true;
   }
 }
--- lib/SignupScreen.dart
+++ lib/SignupScreen.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import './Curvepainter.dart';
 
 class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Esmart"),
+        body: CustomPaint(
+      painter: CurvePainter(),
+      child: Container(
+        margin: EdgeInsets.fromLTRB(25, 5, 25, 0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 140,
+            ),
+            Align(
+              child: Text(
+                "Get on Board",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w900),
+              ),
+              alignment: Alignment.topLeft,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Confirm Password'),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text("Sign Up"),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text("I am already a member"),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
-    );
+    ));
   }
 }
